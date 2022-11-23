aa6821e6fb92a1f7581ffa5dc9d94a24cb17b749
sdggames <cicd@sdggamecollective.com>
2022-11-23 01:50:01 -0600

    Fix dependency on Console

--- FpsCounter.gd
+++ FpsCounter.gd
@@ -12,7 +12,7 @@ extends CanvasLayer
 # Register the FPS counter with a console if the console exists.
 func _ready():
 	if $"/root".has_node("Console"):
-		Console.add_command("fps", self, 'toggle_enabled')\
+		$"/root".get_node("Console").add_command("fps", self, 'toggle_enabled')\
 				.set_description("Enables or disables the FPS counter.")\
 				.register()
 	
